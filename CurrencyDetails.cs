915ba682b5fac6f0be4f39a2ee58f7eb30dc84ad
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2022-08-25 14:47:51 +0300

    Add files via upload

---
+++ CurrencyDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrenciesDB_WFA
+{
+    internal class CurrencyDetails
+    {
+        string Currency;
+        double Buying;
+        double Selling;
+        DateTime Date;
+
+        public string Currency_
+        {
+            get { return Currency; }
+            set { Currency = value; }
+        }
+        public double Buying_
+        {
+            get { return Buying; }
+            set { Buying = value; }
+        }
+        public double Selling_
+        {
+            get { return Selling; }
+            set { Selling = value; }
+        }
+        public DateTime Date_
+        {
+            get { return Date; }
+            set { Date = value; }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+
+namespace CurrenciesDB_WFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (CurrencyDBService.CurrencyDBServiceClient dbServiceClient = new CurrencyDBService.CurrencyDBServiceClient())
+            {
+                var selectedDate = dateTimePicker1.Value;
+                string selectedDateString = selectedDate.ToString("dd/M/yyyy", CultureInfo.InvariantCulture);
+                string selectedCurrency = (string)comboBox1.SelectedItem;
+
+                var currencyDetailsList = dbServiceClient.getCurrenyAsync(selectedDate,selectedCurrency).Result;
+
+                dataGridView1.DataSource = currencyDetailsList;
+
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    MessageBox.Show("İstenilen Özelliklere Ait Veri Bulunamadı..!", "HATA");
+                }
+                else
+                {
+                    dataGridView1.Columns["Date_"].DisplayIndex = 0;
+                    dataGridView1.Columns["Currency_"].DisplayIndex = 1;
+                    dataGridView1.Columns["Buying_"].DisplayIndex = 2;
+                    dataGridView1.Columns["Selling_"].DisplayIndex = 3;
+
+                    dataGridView1.Columns["Date_"].HeaderText = "TARİH";
+                    dataGridView1.Columns["Currency_"].HeaderText = "DÖVİZ CİNSİ";
+                    dataGridView1.Columns["Buying_"].HeaderText = "DÖVİZ ALIŞ";
+                    dataGridView1.Columns["Selling_"].HeaderText = "DÖVİZ SATIŞ";
+                }
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
